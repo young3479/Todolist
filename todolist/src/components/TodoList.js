32a32397ec005e64472a9c146c36a0c62c5698ab
young <chae00901@naver.com>
2023-07-19 23:00:53 +0900

    해당 날짜에 맞는 투두 표시(미완)

--- todolist/src/components/TodoList.js
+++ todolist/src/components/TodoList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import TodoItem from './TodoItem';
-import { useTodoState } from '../TodoContext';
+import { useSelectedDate, useTodoState } from '../TodoContext';
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -12,14 +12,32 @@ const TodoListBlock = styled.div`
 
 function TodoList() {
     const todos = useTodoState();
+    const selectedDate = useSelectedDate();
+
+    const filteredTodos = todos.filter(todo => {
+        const todoDate = new Date(todo.date);
+        const selectedDateOnly = new Date(
+            selectedDate.getFullYear(),
+            selectedDate.getMonth(),
+            selectedDate.getDate()
+        );
+
+        return (
+            todoDate.getFullYear() === selectedDateOnly.getFullYear() &&
+            todoDate.getMonth() === selectedDateOnly.getMonth() &&
+            todoDate.getDate() === selectedDateOnly.getDate()
+        );
+    });
+
     return (
         <TodoListBlock>
-            {todos.map(todo => (
+            {filteredTodos.map(todo => (
                 <TodoItem
                     id={todo.id}
                     text={todo.text}
                     done={todo.done}
                     key={todo.id}
+                    date={todo.date}
                 />
             ))}
         </TodoListBlock>
